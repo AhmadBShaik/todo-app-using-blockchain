1d79ee160f5a950307b83ce409ea973fe98518e5
AhmadBShaik <ahmad.shaik1106@gmail.com>
2022-03-26 11:09:03 +0530

    display todos on frontend using web3

--- components/TodoList.tsx
+++ components/TodoList.tsx
@@ -1,73 +1,56 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import {load} from '../connection/connector'
 
 const TodoList: React.FunctionComponent = () => {
+    const [input, setInput] = useState<string>("")
+    const [refresh, setRefresh] = useState<boolean>(true)
+
+    const [accountAddress, setAccountAddress] = useState<string|null>(null)
+    const [contract, setContract] = useState<any>(null)
+    const [tasks, setTasks] = useState<string[]|null>(null)
+
+    useEffect(
+        () => {
+            if(!refresh) return;
+            setRefresh(false);
+            load().then(e => {
+                setAccountAddress(e.accountAddress)
+                setContract(e.todoContract)
+                setTasks(e.tasks)
+                console.log()
+                console.log(e.accountAddress)
+                console.log(e.todoContract)
+                console.log(e.tasks)
+            })
+        }
+    )
     return (
         <div className='flex flex-col justify-center'>
             <h3 className='flex justify-center text-2xl font-bold my-2 text-orange-500 underline'>Blockchain TodoList</h3>
             <div className='flex border mx-3 my-2 border-green-500 rounded-md'>
-                <input type="text" placeholder='Create a task' className='flex-grow outline-none rounded-l py-1 px-1 font-bold'/>
+                <input type="text" placeholder='Create a task' className='flex-grow outline-none rounded-l py-1 px-1 font-bold'
+                    onChange={(e) => setInput(e.target.value)}
+                />
                 <button className='bg-green-500 text-white px-3 py-1 rounded-r font-bold text-sm'>ADD</button>
             </div>
 
-            <ul className='mt-3'>
-
-                <li className='border border-green-700 flex flex-col mx-3 my-2 rounded p-3 bg-white drop-shadow-sm hover:drop-shadow-lg'>
-                    <div className='flex flex-row'>
-                        <span className='font-bold flex-grow'>todo test</span> 
-                        {/* <button className='font-medium bg-green-500 px-2 py-1 rounded text-white text-sm'>DONE</button> */}
-                        <input type="checkbox" className='appearance indeterminate:bg-gray-300 text-lg'/>
-                    </div>
-                    <div className='flex justify-between mt-3'>
-                        <button className='bg-yellow-500 text-white px-2 py-1 rounded font-medium text-sm'>UPDATE</button>
-                        <button className='bg-red-500 text-white px-2 py-1 rounded font-medium text-sm'>DELETE</button>
-                    </div>
-                </li>
-                <li className='border border-green-700 flex flex-col mx-3 my-2 rounded p-3 bg-white drop-shadow-sm hover:drop-shadow-lg'>
-                    <div className='flex flex-row'>
-                        <span className='font-bold flex-grow'>todo test</span> 
-                        {/* <button className='font-medium bg-green-500 px-2 py-1 rounded text-white text-sm'>DONE</button> */}
-                        <input type="checkbox" className='appearance indeterminate:bg-gray-300 text-lg'/>
-                    </div>
-                    <div className='flex justify-between mt-3'>
-                        <button className='bg-yellow-500 text-white px-2 py-1 rounded font-medium text-sm'>UPDATE</button>
-                        <button className='bg-red-500 text-white px-2 py-1 rounded font-medium text-sm'>DELETE</button>
-                    </div>
-                </li>
-                <li className='border border-green-700 flex flex-col mx-3 my-2 rounded p-3 bg-white drop-shadow-sm hover:drop-shadow-lg'>
-                    <div className='flex flex-row'>
-                        <span className='font-bold flex-grow'>todo test</span> 
-                        {/* <button className='font-medium bg-green-500 px-2 py-1 rounded text-white text-sm'>DONE</button> */}
-                        <input type="checkbox" className='appearance indeterminate:bg-gray-300 text-lg'/>
-                    </div>
-                    <div className='flex justify-between mt-3'>
-                        <button className='bg-yellow-500 text-white px-2 py-1 rounded font-medium text-sm'>UPDATE</button>
-                        <button className='bg-red-500 text-white px-2 py-1 rounded font-medium text-sm'>DELETE</button>
-                    </div>
-                </li>
-                <li className='border border-green-700 flex flex-col mx-3 my-2 rounded p-3 bg-white drop-shadow-sm hover:drop-shadow-lg'>
-                    <div className='flex flex-row'>
-                        <span className='font-bold flex-grow'>todo test</span> 
-                        {/* <button className='font-medium bg-green-500 px-2 py-1 rounded text-white text-sm'>DONE</button> */}
-                        <input type="checkbox" className='appearance indeterminate:bg-gray-300 text-lg'/>
-                    </div>
-                    <div className='flex justify-between mt-3'>
-                        <button className='bg-yellow-500 text-white px-2 py-1 rounded font-medium text-sm'>UPDATE</button>
-                        <button className='bg-red-500 text-white px-2 py-1 rounded font-medium text-sm'>DELETE</button>
-                    </div>
-                </li>
-                <li className='border border-green-700 flex flex-col mx-3 my-2 rounded p-3 bg-white drop-shadow-sm hover:drop-shadow-lg'>
-                    <div className='flex flex-row'>
-                        <span className='font-bold flex-grow'>todo test</span> 
-                        {/* <button className='font-medium bg-green-500 px-2 py-1 rounded text-white text-sm'>DONE</button> */}
-                        <input type="checkbox" className='appearance indeterminate:bg-gray-300 text-lg'/>
-                    </div>
-                    <div className='flex justify-between mt-3'>
-                        <button className='bg-yellow-500 text-white px-2 py-1 rounded font-medium text-sm'>UPDATE</button>
-                        <button className='bg-red-500 text-white px-2 py-1 rounded font-medium text-sm'>DELETE</button>
-                    </div>
-                </li>
-                
-            </ul>
+            {
+                tasks == null ? <div className="flex justify-center">loading tasks...</div> :
+                <ul className='mt-3'>{
+                    tasks.map((task,idx) =>
+                        <li key={idx} className='border border-green-700 flex flex-col mx-3 my-2 rounded p-3 bg-white drop-shadow-sm hover:drop-shadow-lg'>
+                            <div className='flex flex-row'>
+                                <span className='font-bold flex-grow'>{task[1]}</span> 
+                                <input type="checkbox" className='appearance indeterminate:bg-gray-300 text-lg'/>
+                            </div>
+                            <div className='flex justify-between mt-3'>
+                                <button className='bg-yellow-500 text-white px-2 py-1 rounded font-medium text-sm'>UPDATE</button>
+                                <button className='bg-red-500 text-white px-2 py-1 rounded font-medium text-sm'>DELETE</button>
+                            </div>
+                        </li>
+                    )
+                }</ul>   
+            }
         </div>
     )
 }
---
+++ connection/connector.js
@@ -0,0 +1,55 @@
+import TodoListJSON from '../build/contracts/TodoList.json';
+
+const contract = require('@truffle/contract')
+import Web3 from 'web3';
+
+
+export const load = async () => {
+    await loadWeb3();
+    const accountAddress = await loadAccount();
+    const {todoContract, tasks} = await loadContract(accountAddress);
+    return {accountAddress, todoContract, tasks}
+}
+
+
+const loadAccount = async () => {
+    const accountAddress = await web3.eth.getCoinbase();
+    return accountAddress;
+}
+
+const loadTasks = async (contract, accountAddress) => {
+    const tasksCount = await contract.tasksCount(accountAddress);
+    const tasks = [];
+
+    for(let i=0;i<tasksCount;i++){
+        const task = await contract.tasks(accountAddress, i);
+        tasks.push(task);
+    }
+
+    return tasks;
+}
+
+const loadContract = async (accountAddress) => {
+    const theContract = await contract(TodoListJSON);
+    theContract.setProvider(web3.eth.currentProvider);
+
+    const todoContract = await theContract.deployed();
+    const tasks = await loadTasks(todoContract, accountAddress);
+
+    return {todoContract, tasks}
+}
+
+const loadWeb3 = async () => {
+    if(window.ethereum){
+        window.web3 = new Web3(ethereum);
+        try{
+            await ethereum.enable();
+        }catch(e){
+            console.log(e);
+        }
+    }else if(window.web3){
+        window.web3 = new Web3(web3.currentProvider);
+    }else{
+        console.log("Metamask is not installed");
+    }
+}
